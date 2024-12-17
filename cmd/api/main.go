17e781e0529aea534bd0a882818d6b0526afd3f7
rixbur <savecentar@gmail.com>
2024-12-17 11:02:30 +0100

    Move write logic to the helper package
    not sure why the author like helper package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -23,34 +22,18 @@ type application struct {
 }
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	// Create a map which holds the information that we want to send in the response.
 	data := map[string]string{
-		"status": "available", "environment": app.config.env, "version": version,
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
 	}
 
-	// Pass the map to the json.Marshal() function. This returns a []byte slice
-	// containing the encoded JSON. If there was an error, we log it and send the client
-	// a generic error message.
-	js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Print(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
 	}
 
-	// Append a newline to the JSON. This is just a small nicety to make it easier to
-	// view in terminal applications.
-	js = append(js, '\n')
-
-	// At this point we know that encoding the data worked without any problems, so we
-	// can safely set any necessary HTTP headers for a successful response.
-	// Set the "Content-Type: application/json" header on the response.
-	// If you forget to do this, Go will default to sending a "Content-Type: text/plain; charset=utf-8"
-	// header instead.
-	w.Header().Set("Content-Type", "application/json")
-
-	// Use w.Write() to send the []byte slice containing the JSON as the response body.
-	w.Write(js)
 }
 
 func main() {
