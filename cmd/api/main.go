6dfa8e73ff3cb9f185bb21478fd2bb530b7e0475
rixbur <savecentar@gmail.com>
2024-12-26 19:51:26 +0100

    implement custom errors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -29,8 +29,7 @@ func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Reques
 		"version":     version,
 	}, nil)
 	if err != nil {
-		app.logger.Print(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 
 }
